300dc1f68dc589e0dae1234f4a94af964a8f86f1
Dosu333 <vladamir1865@gmail.com>
2021-08-19 21:48:30 +0100

    add static root

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'olmresume.herokuapp.com']
 
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -122,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 STATIC_URL = '/static/'
 
 MEDIA_URL ='/images/'
