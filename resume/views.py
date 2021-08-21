73341e369d3bb26c70996ed0b7a3d47cb42e83d1
Dosu333 <vladamir1865@gmail.com>
2021-08-21 15:14:14 +0100

    added thank you template

--- resume/views.py
+++ resume/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.contrib import messages
 from . import models
 from .forms import ContactForm
 
